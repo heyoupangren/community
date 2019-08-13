f572ab61658e630e337eaa8be811d8e7a0df4e55
heyoupangren <704208837@qq.com>
2019-08-13 13:45:17 +0800

    新建分支，添加视频及视频详情页面

---
+++ src/main/java/life/cwh/community/controller/VideoController.java
@@ -0,0 +1,23 @@
+package life.cwh.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class VideoController {
+    @GetMapping("/videos")
+    public String videos(){
+    return "videos";
+    }
+
+    @GetMapping("/video")
+    public String video(@RequestParam(name = "aid") String aid,
+                        @RequestParam(name = "page",defaultValue = "1") String page,
+                        Model model){
+        model.addAttribute("aid",aid);
+        model.addAttribute("page",page);
+        return "video";
+    }
+}
